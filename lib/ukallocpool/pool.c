a999a183426234be6b02ef40de1d420dcd7e9278
Simon Kuenzer <simon.kuenzer@neclab.eu>
2020-11-18 00:24:13 +0100

    lib/ukallocpool: Always provide `uk_alloc_availmem()`, `uk_alloc_maxalloc()`
    Adopts to the changed meaning of `CONFIG_LIBUKALLLC_IFSTATS` and provide
    `uk_alloc_availmem()` and `uk_alloc_maxalloc()` always.
    
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

--- lib/ukallocpool/pool.c
+++ lib/ukallocpool/pool.c
@@ -208,14 +208,19 @@ void uk_allocpool_return_batch(struct uk_allocpool *p,
 		_prepend_free_obj(p, obj[i]);
 }
 
-#if CONFIG_LIBUKALLOC_IFSTATS
 static ssize_t pool_availmem(struct uk_alloc *a)
 {
 	struct uk_allocpool *p = ukalloc2pool(a);
 
-	return (size_t) p->free_obj_count * p->obj_len;
+	return (ssize_t) (p->free_obj_count * p->obj_len);
 }
-#endif
+
+static ssize_t pool_maxalloc(struct uk_alloc *a)
+{
+	struct uk_allocpool *p = ukalloc2pool(a);
+
+	return (ssize_t) p->obj_len;
+}
 
 size_t uk_allocpool_reqmem(unsigned int obj_count, size_t obj_len,
 			   size_t obj_align)
@@ -300,6 +305,8 @@ out:
 			     pool_free,
 			     pool_posix_memalign,
 			     uk_memalign_compat,
+			     pool_maxalloc,
+			     pool_availmem,
 			     NULL);
 #if CONFIG_LIBUKALLOC_IFSTATS
 	p->self.availmem = pool_availmem;
