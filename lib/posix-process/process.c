e427d83fc7136a3bea4134559ff8539f5eb6f0b0
Simon Kuenzer <simon.kuenzer@neclab.eu>
2021-02-17 14:37:33 +0100

    lib/uksched: Provide & register 'sched_yield' to syscall_shim
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

--- lib/posix-process/process.c
+++ lib/posix-process/process.c
@@ -36,6 +36,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/prctl.h>
 #include <sys/resource.h>
 #include <uk/process.h>
@@ -441,6 +442,16 @@ UK_SYSCALL_R_DEFINE(int, setrlimit, int, resource, const struct rlimit *, rlim)
 {
 	return uk_syscall_r_prlimit64(0, (long) resource,
 				      (long) rlim, (long) NULL);
+}
+
+UK_SYSCALL_R_DEFINE(int, getrusage, int, who,
+		    struct rusage *, usage)
+{
+	if (!usage)
+		return -EFAULT;
+
+	memset(usage, 0, sizeof(*usage));
+	return 0;
 }
 
 #if UK_LIBC_SYSCALLS
